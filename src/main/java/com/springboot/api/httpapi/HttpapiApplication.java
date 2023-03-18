7be87d47f8861ad76ea633dcb77ff04a5506a45b
srinath <shankarsrinath@gmail.com>
2023-03-18 22:54:31 +0530

    Refactor: sk - dbconn string and logging

--- src/main/java/com/springboot/api/httpapi/HttpapiApplication.java
+++ src/main/java/com/springboot/api/httpapi/HttpapiApplication.java
@@ -1,18 +1,40 @@
 package com.springboot.api.httpapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @RestController
 @SpringBootApplication
 public class HttpapiApplication {
 
+	@Autowired
+	DataSource dataSource;
+
+	private final Logger LOGGER = LogManager.getLogger(this.getClass());
+
 	@GetMapping("/live")
-	public String message(){
-		return "Well done";
+	public String checkDBStatus (){
+		try {
+			LOGGER.info("Database Connection establish...");
+			Connection connection = dataSource.getConnection();
+			LOGGER.info("Database Connection established.");
+			return "Well done";
+		} catch (SQLException e) {
+			LOGGER.debug("Database Connection issue...");
+			return "Maintenance";
+		}
+
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HttpapiApplication.class, args);
 	}
