2f66aabd1f009b6384e15c9380e4ef9b440d1083
srinath <shankarsrinath@gmail.com>
2023-03-18 19:33:21 +0530

    initial commit

---
+++ src/test/java/com/springboot/api/httpapi/HttpapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.api.httpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
